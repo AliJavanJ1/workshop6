8b7d00d4ef59a0305dffb48641237af13fa4787f
AliJavanJ1 <aj.ali.javan@gmail.com>
2022-12-03 21:23:15 +0330

    Revert "add division to main"
    This reverts commit b92dbf8ca13efc01ac37097c0f492ae9d3046252.

--- main.c
+++ main.c
@@ -1,9 +1,5 @@
 #include "stdio.h"
 
 int main() {
-    int a, b;
-    scanf("%d", &a);
-    printf("/\n");
-    scanf("%d", &b);
-    printf("=\n%f\n", a / (float) b);
+    printf("Hello World!");
 }
